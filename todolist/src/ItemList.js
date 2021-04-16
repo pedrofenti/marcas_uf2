c1a04a415d3bd9f6077ad2ac9054d60256696cc5
pedrofenti <pedro.fernandez@enti.cat>
2021-04-16 14:32:57 +0200

    Borrando elementos

--- todolist/src/ItemList.js
+++ todolist/src/ItemList.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'; //importa React y el componente del modulo
 
 class ItemList extends Component { //clase ItemList que hereda de Component
-  render() {
+  
+	constructor (props){
+		super(props);
+
+		this.removeItem = this.removeItem.bind(this);
+	}
+
+	removeItem(){
+		this.props.parentRemove(this.props.id_item);
+	}
+	
+	render() {
     return (
-      <li>{this.props.item} <button>X</button></li>
+      <li>{this.props.item} <button className="delete"
+			onClick={this.removeItem}>X</button></li>
     );
   }
 }
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,48 +1,89 @@
 import React, { Component } from 'react'; //importa React y el componente del modulo
 import ItemList from './ItemList';
 
+import ".TodoList.css"
 
 class TodoList extends Component { //clase TodoList que hereda de Component
 	
-	constructor (props) {
+	//constructor del objeto
+	constructor (props) { 
 		
-		super(props);
+		//super es el padre
+		super(props); 
 
+		//estado
 		this.state = {
 			items : []
 		};
+		
+		//variable para las ids
+		this.last_id = 0;
 
+    //vinculamos la funcion addItem a nuestro objeto
 		this.addItem = this.addItem.bind(this);
+		this.removeItem = this.removeItem.bind(this);
+
+		}
+	
+	//funcion para anyadir en la lista
+	addItem (e) {
+
+		//hace que no se envie el formulario por defecto
+		e.preventDefault();
+	
+		//cogemos lo que haya en el formulario
+		let text_v = document.getElementById("text-task").value;
+		document.getElementById("text-task").value = "";
+		document.getElementById("text-task").focus();
+
+		this.last_id++;
+	
+		//hacemos el push de su valor en el array del estado
+		this.state.items.push({id: this.last_id, item:text_v});
+	
+		//funcion para realizar los cambios (internamente llamamos a render();)
+		this.setState({ items: this.state.items  });	
+		}
+
+	removeItem(id_item) {
+	
+		console.log("Remove" + id_item);	
+		//bucle para encontrar la casilla dentro del array cn ese id
+		//triple igual, para igual el tipo tmb
+		//splice para borrar tanto el contenido como la casilla del array, solo 1
+
+		for (let i = 0; i < this.state.items.length; i++) {
+			if (this.state.items[i].id === id_item){
+				this.state.items.splice(i,1);
+				break;
+			}
+		}	
+	
+		//funcion para realizar los cambios (internamente llamamos a render();)
+		this.setState({ items: this.state.items  });	
+	
 
 	}
-		
-	addItem (e) {
-	e.preventDefault();
 	
-	let text_v = document.getElementById("text-task").value;
-
-	this.state.items.push({id:1, item:text_v});
-
-	this.setState({ items: this.state.items  })
-
-}
-
-render() {
+	render() {
 	
+		//map es un bucle del estado
 		let list = this.state.items.map( (todo_item) => {
-			return (<ItemList item={todo_item.item}/>);
+			return (<ItemList item={todo_item.item}
+												id_item={todo_item.id}
+												parentRemove={this.removeItem}/>);
 		});
 	
     return (
       <div className="TodoList">
-      	<h1>Welcome to your list</h1>
-        <ul>
+        <p>Num Items: NUM</p>
+				<form onSubmit={this.addItem}>
+				<p><input type="text" id="text-task" />
+				<button type="submit">Añadir</button></p>
+				</form>
+				<ul>
 					{list}					
 				</ul>
-				<form onSubmit={this.addItem}>
-				<p><input type="text" id="text-task" /></p>
-				<p><button type="submit">Añadir</button></p>
-				</form>
       </div>
     );
   }
